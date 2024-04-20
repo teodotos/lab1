4ae2315c79e10732eccba4d0a9724a9eaafeca67
teodotos <167648528+teodotos@users.noreply.github.com>
2024-04-20 23:17:42 +0300

    Add files via upload

---
+++ lab1/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,6 @@
+package org.example;
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
